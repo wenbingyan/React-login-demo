46dade431eaef772063108f379db52389ee3c29b
温冰炎 <663648512@qq.com>
2018-09-25 15:19:58 +0800

    asd1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+
 import { ADD, MINUS,ASYNC  } from './index.redux';
 
 @connect(
@@ -12,16 +13,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>there are account is1 { this.props.num.aaa }</h1>  
-        <button onClick={this.props.ADD}>+</button>
-        <button onClick={this.props.MINUS}>-</button>
-        <button onClick={this.props.ASYNC}>LATER +</button>
-      </div>
+        <div>
+          <h1>there are account is1 { this.props.num.aaa }</h1>  
+          <button onClick={this.props.ADD}>+</button>
+          <button onClick={this.props.MINUS}>-</button>
+          <button onClick={this.props.ASYNC}>LATER +</button>
+        </div>
+      </div>    
     );
   }
 }
 
 export default App;
+
+
+
 //  1 npm run eject
 //  2 npm install babel-plugin-transform-decorators-legacy --save-dev
 //  3 packages.json 
